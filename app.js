50725033cb19d6b30fa155de47959cb75d944a4c
Bartomiej <b.giepard@donedeliveries.com>
2024-10-19 12:31:07 +0200

    server 3

--- app.js
+++ app.js
@@ -9,45 +9,45 @@ server.listen(process.env.PORT || 3000, () => {
   console.log('Serwer nasłuchuje na porcie 3000');
 });
 
-function generateRoomID() {
-  return Math.random().toString(36).substr(2, 6).toUpperCase();
-}
-
-io.on('connection', (socket) => {
-  console.log('Użytkownik połączony:', socket.id);
-
-  socket.on('createRoom', (data, callback) => {
-    const roomID = generateRoomID();
-    socket.join(roomID);
-    socket.roomID = roomID;
-    socket.gameOptions = data; // np. { rounds: 5, categories: ['A', 'B'], maxPlayers: 4 }
-    socket.players = [socket.id];
-    callback({ roomID });
-  });
-
-  socket.on('joinRoom', (roomID, callback) => {
-    const room = io.sockets.adapter.rooms.get(roomID);
-    if (room && room.size < socket.gameOptions.maxPlayers) {
-      socket.join(roomID);
-      socket.roomID = roomID;
-      socket.players.push(socket.id);
-      io.to(roomID).emit('playerJoined', { playerID: socket.id });
-
-      if (room.size === socket.gameOptions.maxPlayers) {
-        io.to(roomID).emit('startGame', { message: 'Gra się rozpoczyna!' });
-      }
-      callback({ success: true });
-    } else {
-      callback({ success: false, message: 'Pokój pełny lub nie istnieje.' });
-    }
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Użytkownik rozłączony:', socket.id);
-  });
-
-  socket.on('gameAction', (action) => {
-    const roomID = socket.roomID;
-    io.to(roomID).emit('gameData', { /* dane gry */ });
-  });
-});
+// function generateRoomID() {
+//   return Math.random().toString(36).substr(2, 6).toUpperCase();
+// }
+//
+// io.on('connection', (socket) => {
+//   console.log('Użytkownik połączony:', socket.id);
+//
+//   socket.on('createRoom', (data, callback) => {
+//     const roomID = generateRoomID();
+//     socket.join(roomID);
+//     socket.roomID = roomID;
+//     socket.gameOptions = data; // np. { rounds: 5, categories: ['A', 'B'], maxPlayers: 4 }
+//     socket.players = [socket.id];
+//     callback({ roomID });
+//   });
+//
+//   socket.on('joinRoom', (roomID, callback) => {
+//     const room = io.sockets.adapter.rooms.get(roomID);
+//     if (room && room.size < socket.gameOptions.maxPlayers) {
+//       socket.join(roomID);
+//       socket.roomID = roomID;
+//       socket.players.push(socket.id);
+//       io.to(roomID).emit('playerJoined', { playerID: socket.id });
+//
+//       if (room.size === socket.gameOptions.maxPlayers) {
+//         io.to(roomID).emit('startGame', { message: 'Gra się rozpoczyna!' });
+//       }
+//       callback({ success: true });
+//     } else {
+//       callback({ success: false, message: 'Pokój pełny lub nie istnieje.' });
+//     }
+//   });
+//
+//   socket.on('disconnect', () => {
+//     console.log('Użytkownik rozłączony:', socket.id);
+//   });
+//
+//   socket.on('gameAction', (action) => {
+//     const roomID = socket.roomID;
+//     io.to(roomID).emit('gameData', { /* dane gry */ });
+//   });
+// });
